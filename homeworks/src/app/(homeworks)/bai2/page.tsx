d116b13c3a683d399a0161996f0ecec0ebfab86a
DoNhatNam1 <namnhatvt2595@gmail.com>
2024-01-26 16:21:48 +0700

    first commit

---
+++ homeworks/src/app/(homeworks)/bai2/page.tsx
@@ -0,0 +1,8 @@
+
+export default function Bai2() {
+  return (
+    <>
+
+    </>
+  );
+}
---
+++ homeworks/src/app/(homeworks)/page.tsx
@@ -0,0 +1,11 @@
+
+import Link from 'next/link'
+
+export default function Home() {
+  return (
+    <>
+    <Link href="/bai1">Bài 1</Link>
+    <Link href="/bai2">Bài 2</Link>
+    </>
+  );
+}
