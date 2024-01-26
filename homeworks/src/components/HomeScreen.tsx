ba7ebf46557796e4eed84b67262a22b6411e36f1
DoNhatNam1 <namnhatvt2595@gmail.com>
2024-01-26 20:17:45 +0700

    style: :lipstick: Update HomeScreen in Components

--- homeworks/src/components/HomeScreen.tsx
+++ homeworks/src/components/HomeScreen.tsx
@@ -4,19 +4,24 @@ import Link from "next/link";
 
 export const HomeScreen = () => {
   return (
-    <>
-      <Link 
+    <div className="w-full h-96 grid place-content-center gap-5">
+    <Link 
       href="/bai1" 
-      className="p-5 bg-blue"
+      className="px-8 py-2 bg-purple-950 hover:bg-purple-500 text-white rounded-xl"
       >
+    <button className="w-full h-full p-2">
         Bài 1
+    </button>
       </Link>
-      <Link 
+
+    <Link 
       href="/bai2" 
-      className="p-5 bg-blue"
+      className="px-8 py-2 bg-purple-950 hover:bg-purple-500 text-white rounded-xl"
       >
+    <button className="w-full h-full p-2">
         Bài 2
+    </button>
       </Link>
-    </>
+    </div>
   );
 };
