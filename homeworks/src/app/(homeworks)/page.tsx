5d8e9879406ea57eb71319e3b4cae0cdfbf8c167
DoNhatNam1 <namnhatvt2595@gmail.com>
2024-01-26 21:29:24 +0700

    refactor: :sparkles: Test Deploy

--- homeworks/src/app/(homeworks)/page.tsx
+++ homeworks/src/app/(homeworks)/page.tsx
@@ -2,6 +2,10 @@
 import { HomeScreen } from '@/components/HomeScreen';
 
 export default function Home() {
+  const BASE_URL = process.env.NEXT_PUBLIC_BASE_API_URL
+  if(!BASE_URL){
+    return null;
+  }
   return (
     <>
     <HomeScreen/>
